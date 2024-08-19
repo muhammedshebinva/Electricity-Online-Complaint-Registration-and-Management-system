9caf21042f874f703121a6444d4a542fea1feb86
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-08-19 22:09:29 +0530

    home page css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import AuthContext from '../provider/authContext'
 import { useNavigate } from 'react-router-dom'
-
+import '../App.css'
 function Home() {
   const {setLastRoute} = useContext(AuthContext)
   useEffect(() => {
@@ -14,16 +14,23 @@ function Home() {
   const navigate = useNavigate()
   
   return (
-    <div>
-    {token ? <h1>Home</h1> :
+    <div >
+    <div className='home-page-main'> </div>
     
+    {token ? <h1>Home</h1> :
     <div>
-    <button onClick={()=>navigate('/login')}>User Login</button> <br></br>
-    <button onClick={()=>navigate('/officer/login')}>Officer Login</button> <br></br>
-    <button onClick={()=>navigate('/admin/login')}>Admin Login</button> <br></br>
+    <div className='home-control-div'>
+    <h1>register Your Complaints</h1>
+    <p>This platform enables you to register your complaints</p>
+    </div>
+    <div className='home-button-div'>
+    <button className='home-button' onClick={()=>navigate('/login')}>User Login</button> <br></br>
+    <button className='home-button' onClick={()=>navigate('/officer/login')}>Officer Login</button> <br></br>
+    <button className='home-button' onClick={()=>navigate('/admin/login')}>Admin Login</button> <br></br>
+    </div>
     </div>
     }
-
+   
     </div>
   )
 }
